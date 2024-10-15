ba5b92b88bac452e23172a1aa8bfe319907c488f
Phaedra144 <bedi.szilvia@gmail.com>
2024-10-15 13:19:03 +0200

    Fix Footer at the bottom, fix some style on TodoContainer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-midnight-navy text-ice-white py-4 absolute bottom-0 w-full">
+    <footer className="bg-midnight-navy text-ice-white py-4 bottom-0 w-full">
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} Todo App with Timer</p>
       </div>
--- src/TodoContainer.tsx
+++ src/TodoContainer.tsx
@@ -3,11 +3,13 @@ import React from "react";
 export const TodoContainer = ({ children }) => {
   return (
     <div className="bg-ice-white min-h-screen p-4 sm:p-6 md:p-8">
-      <div className="max-w-md mx-auto bg-cloudy-sky rounded-lg shadow-lg overflow-hidden">
-        <h1 className="text-2xl font-bold text-midnight-navy bg-ice-white p-4 border-b-2 border-midnight-navy">
+      <div className="max-w-4xl mx-auto overflow-hidden">
+        <h1 className="text-2xl font-bold text-midnight-navy bg-ice-white p-4 border-midnight-navy">
           Todo List
         </h1>
-        <div className="p-4">{children}</div>
+        <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {children}
+        </div>
       </div>
     </div>
   );
